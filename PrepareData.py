179e26b110b1361d0105562dbe03895951e65cf1
Leonardo Neves <enfimleo@gmail.com>
2016-06-21 03:08:36 -0300

    refactorando e melhorando o codigo

--- PrepareData.py
+++ PrepareData.py
@@ -10,18 +10,18 @@ import numpy as np
 # 1         2           3           4               5           6               7               8       9
 # Name    DateTime    OutcomeType OutcomeSubtype  AnimalType SexuponOutcome  AgeuponOutcome  Breed   Color
 #   3       x           5           16              2           2   + 3            9999         2       2
- 
+
+
+#-------------------------PrepareData 
 class PrepareData:
-
-
-
+    X=[]
+    y = []
+    y2 = []
     def __init__(self,file):
         x = []
         #x.append("zero")
         #x.extend(["AnimalID","Name","DateTime","AnimalType","SexuponOutcome","AgeuponOutcome","Breed","Color"])
         #print(x[0:8])
-        y = []
-        y2 = []
 
         with open(file, 'r') as csv_file:
             train = csv.reader(csv_file, delimiter=',')
@@ -39,23 +39,26 @@ class PrepareData:
                     x.extend ( [self.Color1(row[9])] )
                     x.extend ( [self.Color2(row[9]) ] )
 
-                    y.append ( self.OutcomeType(row[3]) )
-                    y2.append ( self.OutcomeSubtype(row[4]) )
+                    self.y.append ( self.OutcomeType(row[3]) )
+                    self.y2.append ( self.OutcomeSubtype(row[4]) )
                 #endif
             #endfor 
         #endwith
-        X=np.array(x).reshape(len(x)/9,9)
+        self.X=np.array(x).reshape(len(x)/9,9)
+        #self.Printer(len(x)/9)
 
-        for i in xrange(len(x)/9):
-            print(X[i]) #matrix
-            print(y[i]) #retorno_esperado #OutcomeType
-            print(y2[i]) #retorno_esperado2 #OutcomeSubType
-            print("\n")
     #end__init__
 
+    def Printer(self,lines):
+        for i in xrange(lines):
+            print(self.X[i]) #matrix
+            print(self.y[i]) #retorno_esperado #OutcomeType
+            print(self.y2[i]) #retorno_esperado2 #OutcomeSubType
+            print("\n")
 
-#SWITCHS
-#http://www.pydanny.com/why-doesnt-python-have-switch-case.html
+
+    #SWITCHS
+    #http://www.pydanny.com/why-doesnt-python-have-switch-case.html
     def Name(self,value):
         if value is '':
             return 
@@ -175,13 +178,125 @@ class PrepareData:
 
 
 
+
+
+#-------------------------Perceptron
+class Perceptron:
+
+    def __init__(self, amostras, saidas, taxa_aprendizado=0.1, epocas=1000, limiar=-1):
+
+        self.amostras = amostras # todas as amostras
+        self.saidas = saidas # saídas respectivas de cada amostra
+        self.taxa_aprendizado = taxa_aprendizado # taxa de aprendizado (entre 0 e 1)
+        self.epocas = epocas # número de épocas
+        self.limiar = limiar # limiar
+        self.num_amostras = len(amostras) # quantidade de amostras
+        self.num_amostra = len(amostras[0]) # quantidade de elementos por cada amostra
+        self.pesos = [] # vetor dos pesos
+
+
+    # função utilizada para treinar a rede
+    def treinar(self):
+
+        # adiciona -1 para cada amostra
+        for amostra in self.amostras:
+            amostra.insert(0, -1)
+
+        # inicia o vetor de pesos com valores aleatórios pequenos
+        for i in range(self.num_amostra):
+            self.pesos.append(random.random())
+
+        # insere o limiar no vetor de pesos
+        self.pesos.insert(0, self.limiar)
+
+        num_epocas = 0 # inicia o contador de épocas
+        
+        while True:
+
+            erro = False # erro inicialmente inexiste
+
+            # para todas as amostras de treinamento
+            for i in range(self.num_amostras):
+                
+                u = 0
+                '''
+                    realiza o somatório, o limite (self.num_amostra + 1) 
+                    é porque foi inserido o -1 em cada amostra
+                '''
+                for j in range(self.num_amostra + 1):
+                    u += self.pesos[j] * self.amostras[i][j]
+
+                # obtém a saída da rede utilizando a função de ativação
+                y = self.sinal(u)
+
+                # verifica se a saída da rede é diferente da saída desejada
+                if y != self.saidas[i]:
+
+                    # calcula o erro: subtração entre a saída desejada e a saída da rede
+                    erro_aux = self.saidas[i] - y
+
+                    # faz o ajuste dos pesos para cada elemento da amostra
+                    for j in range (self.num_amostra + 1):
+                        self.pesos[j] = self.pesos[j] + self.taxa_aprendizado * erro_aux * self.amostras[i][j]
+
+                    erro = True # se entrou, é porque o erro ainda existe
+            
+            num_epocas += 1 # incrementa o número de épocas
+
+            # critério de parada é pelo número de épocas ou se não existir erro
+            if num_epocas > self.epocas or not erro:
+                break
+
+
+    # função utilizada para testar a rede
+    # recebe uma amostra a ser classificada e os nomes das classes
+    # utiliza função sinal, se é -1 então é classe1, senão é classe2
+    def testar(self, amostra, classe1, classe2):
+
+        # insere o -1
+        amostra.insert(0, -1)
+
+        '''
+            utiliza-se o vetor de pesos ajustado
+            durante o treinamento da rede
+        '''
+        u = 0
+        for i in range(self.num_amostra + 1):
+            u += self.pesos[i] * amostra[i]
+
+        # calcula a saída da rede
+        y = self.sinal(u)
+
+        # verifica a qual classe pertence
+        if y == -1:
+            print('A amostra pertence a classe %s' % classe1)
+        else:
+            print('A amostra pertence a classe %s' % classe2)
+
+
+    def degrau(self, u):
+        return 1 if u >= 0 else 0
+
+
+    def sinal(self, u): # é a mesma função degrau bipolar
+        return 1 if u >= 0 else -1
+
+
+
+
+
 #========================= MAIN.EXEC =========================#
+
+
 
 microtrain =  "microtrain.csv"   
 train = "../data/shelter_animal/train.csv"
 train = "train.csv"
 test = "../data/shelter_animal/test.csv"
-PrepareData(microtrain)
+data = PrepareData(microtrain)
+print(data.X)
+#print(data.y)
+#print(data.y2)
 
 
 
@@ -271,3 +386,4 @@ Whippet
 Yorkshire Terrier
 Yorkshire Terrier 
 """
+
